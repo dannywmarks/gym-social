d4c556368ae6e182ba9ea141c23383a42ac4cfbb
Danny Marks <dannywmarks@gmail.com>
2022-09-08 15:34:16 -0700

    Skeleton Components, Nav, Side, Home

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useState, useEffect } from "react";
+import { isAssetError } from "next/dist/client/route-loader";
+import NavBar from "../components/NavBar";
+import SideBar from "../components/SideBar";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setIsSSR] = useState(true);
 
-export default MyApp
+  useEffect(() => {
+    setIsSSR(false);
+  }),
+    [];
+
+  if (isSSR) return null;
+
+  return (
+    <div>
+      <NavBar />
+      <div className="flex gap-6 md:gap-20">
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+          <SideBar />
+        </div>
+        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyApp;
